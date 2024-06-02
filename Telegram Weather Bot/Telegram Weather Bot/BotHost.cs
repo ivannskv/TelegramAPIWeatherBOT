4c09d8f322046d19395fa2b3d8299f875138f16c
K1tsune <60034144+Kitsune424@users.noreply.github.com>
2024-06-02 20:32:00 +0300

    Added support for telegram API
    Code can host Telegram bot https://t.me/TRPO_weather_bot
    User can send message and the bot will respond to commands. Commands:
    /start
    /Погода

---
+++ Telegram Weather Bot/Telegram Weather Bot/BotHost.cs
@@ -0,0 +1,45 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Telegram_Weather_Bot
+{
+    class BotHost
+    {
+        public Action<ITelegramBotClient, Update>? OnMessage;
+        private TelegramBotClient _client;
+
+        public BotHost(string token)
+        {
+            _client = new TelegramBotClient(token);
+        }
+
+        public void Start()
+        {
+            _client.StartReceiving(UpdateHandler, ErrHandler);
+            Console.WriteLine("Bot is start");
+        }
+
+        private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message?.Text == null)
+                return;
+            else 
+            {
+                Console.WriteLine($"Chat ID: {update.Message.Chat.Id}" +
+                    $"\nFirstname: {update.Message.Chat.FirstName}" +
+                    $"\nUsername: {update.Message.Chat.Username}" +
+                    $"\nNew message: {update.Message?.Text ?? "[IsNotTextMessage]"}\n");
+
+                OnMessage?.Invoke(client, update);
+            }
+            await Task.CompletedTask;
+        }
+
+        private async Task ErrHandler(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            Console.WriteLine($"Err: " + exception.Message);
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Telegram Weather Bot/Telegram Weather Bot/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using static System.Net.WebRequestMethods;
+
+namespace Telegram_Weather_Bot
+{
+    class Program
+    {
+        private static string weatherAPI { get; set; } = "https://api.openweathermap.org/data/2.5/weather?q=Kostroma&appid=6de2b75f56ae6a8fd6715a26f9ac2f15";
+        private static string token { get; set; } = "7490036689:AAGZsfMTOuSareAbfoLmcfNZmWs4t7_Kh4Q";
+        private static BotHost weatherBot;
+        private static HttpClientHandler httpClientHandler;
+        private static HttpClient httpClent;
+
+
+        private static void Main(string[] args)
+        {
+            httpClientHandler = new HttpClientHandler();
+            httpClientHandler.UseDefaultCredentials = true;
+            httpClent = new HttpClient(httpClientHandler);
+
+            weatherBot = new BotHost(token);
+            weatherBot.Start();
+            weatherBot.OnMessage += OnMessage;
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Ответ пользователю
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="update"></param>
+        private static async void OnMessage(ITelegramBotClient client, Update update)
+        {
+            string command = update.Message?.Text?? "1";
+            switch (command)
+            {
+                case "/start":
+                    await client.SendTextMessageAsync(update.Message?.Chat.Id ?? 654530825,
+                        "Привет! Я Weather - костромской синоптик. Я буду показывать тебе погоду на текущий момент в Костроме.");
+                    break;
+
+                case "/Погода":
+                    await client.SendTextMessageAsync(update.Message?.Chat.Id ?? 654530825, "В будущем тут будет погода");
+                    break;
+            }
+        }
+    }
+}
