41fe5970e92a837397a365657579c8c0c037ae20
K1tsune <60034144+Kitsune424@users.noreply.github.com>
2024-06-02 22:50:10 +0300

    Add OpenWeatherAPI connection
    Но оно не работает и я не знаю почему

--- Telegram Weather Bot/Telegram Weather Bot/Program.cs
+++ Telegram Weather Bot/Telegram Weather Bot/Program.cs
@@ -1,30 +1,49 @@
 ﻿using System;
-using System.Net;
+using System.Text.Json;
+using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Types;
-using static System.Net.WebRequestMethods;
 
 namespace Telegram_Weather_Bot
 {
     class Program
     {
-        private static string weatherAPI { get; set; } = "https://api.openweathermap.org/data/2.5/weather?q=Kostroma&appid=6de2b75f56ae6a8fd6715a26f9ac2f15";
+        private static DateTime currentDate;
+        private static Timer timer;
+
+        static string weatherAPI = "6de2b75f56ae6a8fd6715a26f9ac2f15";
+        static string city = "Kostroma";
+        static string lat = "57.7708";
+        static string lon = "40.9344";
+        static string lang = "ru";
+
         private static string token { get; set; } = "7490036689:AAGZsfMTOuSareAbfoLmcfNZmWs4t7_Kh4Q";
         private static BotHost weatherBot;
-        private static HttpClientHandler httpClientHandler;
-        private static HttpClient httpClent;
 
 
         private static void Main(string[] args)
         {
-            httpClientHandler = new HttpClientHandler();
-            httpClientHandler.UseDefaultCredentials = true;
-            httpClent = new HttpClient(httpClientHandler);
+            currentDate = DateTime.Today;
+            timer = new Timer(UpdateDate, null, TimeSpan.Zero, TimeSpan.FromMinutes(10));
+
 
             weatherBot = new BotHost(token);
             weatherBot.Start();
             weatherBot.OnMessage += OnMessage;
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// обновление даты для токена
+        /// </summary>
+        /// <param name="state"></param>
+        private static void UpdateDate(object? state)
+        {
+            DateTime today = DateTime.Today;
+            if(currentDate != today)
+            {
+                currentDate = today;
+            }
         }
 
         /// <summary>
@@ -35,6 +54,7 @@ namespace Telegram_Weather_Bot
         private static async void OnMessage(ITelegramBotClient client, Update update)
         {
             string command = update.Message?.Text?? "1";
+            command = command.ToLower();
             switch (command)
             {
                 case "/start":
@@ -42,10 +62,50 @@ namespace Telegram_Weather_Bot
                         "Привет! Я Weather - костромской синоптик. Я буду показывать тебе погоду на текущий момент в Костроме.");
                     break;
 
-                case "/Погода":
-                    await client.SendTextMessageAsync(update.Message?.Chat.Id ?? 654530825, "В будущем тут будет погода");
+                case "/текущая погода":
+                case "текущая погода":
+                    await client.SendTextMessageAsync(update.Message?.Chat.Id ?? 654530825, GetWeatherInfo());
                     break;
             }
+        }
+
+        private static string GetWeatherInfo()
+        {
+            string message = GetRespone().GetAwaiter().GetResult();
+            return message;
+        }
+
+        private static async Task<string> GetRespone()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string date = currentDate.ToString("yyyy-MM-dd");
+                string url = $"https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=60.45&lon=-38.67&date=2023-03-30&tz=+03:00&appid={weatherAPI}";
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    
+                }
+
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                WeatherInfo weather = JsonSerializer.Deserialize<WeatherInfo>(jsonResponse);
+
+                if (weather != null)
+                {
+                    string weatherInfo = $"Погода в короде на момент {weather.date} {weather.tz}.\n" +
+                        $"☁️Облачность после полудня: {weather?.cloud_cover?.afternoon}\n" +
+                        $"💧Влажность после полудня: {weather?.humidity?.afternoon}%\n" +
+                        $"🌧Общее кол-во осадков: {weather?.precipitation?.total} мм\n" +
+                        $"🌡Температура макс/мин: {weather?.temperature?.max}/{weather?.temperature?.min}°C\n" +
+                        $"Температура ☀️днем/🌙ночью: {weather?.temperature?.morning}/{weather?.temperature?.night}°C\n" +
+                        $"Давление: {weather?.pressure?.afternoon} гП\n" +
+                        $"🌬Скорость ветра: {weather.wind?.max.speed} м/с, направление {weather.wind?.max.direction}°";
+                    return weatherInfo;
+                }
+            }
+            
+            return "не удалость получить данные о погоде";
         }
     }
 }
---
+++ Telegram Weather Bot/Telegram Weather Bot/WeatherInfo.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Telegram_Weather_Bot
+{
+    public class WeatherInfo
+    {
+        public float lat { get; set; }
+        public float lon { get; set; }
+        public string tz { get; set; }
+        public string date { get; set; }
+        public string units { get; set; }
+        public CloudCover cloud_cover { get; set; }
+        public Humidity humidity { get; set; }
+        public Precipitation precipitation { get; set; }
+        public Temperature temperature { get; set; }
+        public Pressure pressure { get; set; }
+        public Wind wind { get; set; }
+    }
+
+    public class CloudCover
+    {
+        public string afternoon { get; set; }
+    }
+
+    public class Humidity
+    {
+        public string afternoon { get; set; }
+    }
+
+    public class Precipitation
+    {
+        public string total { get; set; }
+    }
+
+    public class Temperature
+    {
+        public string min { get; set; }
+        public string max { get; set; }
+        public string afternoon { get; set; }
+        public string night { get; set; }
+        public string evening { get; set; }
+        public string morning { get; set; }
+    }
+
+    public class Pressure
+    {
+        public string afternoon { get; set; }
+    }
+
+    public class Wind
+    {
+        public Max max { get; set; }
+    }
+
+    public class Max
+    {
+        public string speed { get; set; }
+        public string direction { get; set; }
+    }
+}
